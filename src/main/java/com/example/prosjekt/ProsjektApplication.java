cb63f0915f90d805a4c49082dd566b1ef88efb54
s362124 <s362124@oslomet.no>
2022-08-23 12:47:57 +0200

    Initial commit

---
+++ src/main/java/com/example/prosjekt/ProsjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.prosjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProsjektApplication.class, args);
+    }
+
+}
